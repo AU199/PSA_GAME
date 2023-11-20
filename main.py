75bddef8d32d9ade86d9783b83a1e16b5d06bf50
baller <akulgmvl@gmail.com>
2023-11-20 15:23:08 +0000

    Started the line breaking process

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ screen = pygame.display.set_mode((700, 600))
 pygame.display.set_caption('GAME')
 screen.fill((240, 240, 240))
 x = 0
-g_x = 0
+g_x = 60
 y = 500
-g_y = 0
+g_y = 500
 rounds_move = 1
 rounds_idle = 0
 ax_move = 1
@@ -33,10 +33,17 @@ images_e = []
 images_ax = []
 images_g = []
 images_h = []
+line_breaks = 0
 current_tick = [
-    "right", 600, "axe_swing", 2,"entry_god",2, "speak", "STOP!!", "god", "speak",
-    "Why are you cutting a tree down", "god", "speak",
-    "So that I can graze my cattle", "human", "speak", "", "", "clear", "end"
+    "right", 600, "axe_swing", 2, "entry_god", 2, "speak", "STOP!!", "god",
+    "speak", "Why are you cutting a tree down?", "god", "speak",
+    "So that I can graze my cattle", "human", "speak",
+    " Why can't you do that somewhere else? ", "god", "speak",
+    "Because this is closer.", "human", "speak",
+    "And why does this even matter?", "human", "speak",
+    "Why does this matter!!", "god", "speak",
+    "Well, for starters, It breaks the Carbon Cycle in the forest", "god"
+    "clear", "end"
 ]
 god_current_tick = ["wait", 1000, "left", 20]
 curr_t = current_tick[1]
@@ -131,7 +138,7 @@ def tick(rm, cm, ci, ri, am, ac, gm, gc):
 
     cm += 10
   if gm > len(images_e) - 1:
-    gm =0
+    gm = 0
     gc += 100
 
   if cm > 0:
@@ -140,9 +147,9 @@ def tick(rm, cm, ci, ri, am, ac, gm, gc):
     ci -= 1
   if ac > 0:
     ac -= 1
-  if gc >0:
-    gc -=1 
-  return rm, ri, cm, ci, ac, am, gm,gc
+  if gc > 0:
+    gc -= 1
+  return rm, ri, cm, ci, ac, am, gm, gc
 
 
 def idle(r):
@@ -200,11 +207,21 @@ def head(talker):
     return None
   return pygame.transform.rotozoom(image, 0, 3)
 
-def god_enter(r,y):
-  image = images_g[r]
+
+def god_enter(move, y):
+  image = images_g[0]
   image = pygame.transform.rotozoom(image, 0, 3)
-  image = pygame.transform.flip(image,True,False)
   return image
+
+
+def line_break(curr_sentence: str):
+  if len(curr_sentence) > 20:
+    middle_index = len(curr_sentence) // 2
+    first_sentence = curr_sentence[:middle_index]
+    second_sentence = curr_sentence[middle_index:]
+    return (first_sentence, second_sentence)
+  else:
+    return curr_sentence
 
 
 a = None
@@ -212,6 +229,9 @@ image = None
 lenr = len(current_tick) - 2
 tree = print_tree()
 tree_created = 1
+line_breaks = 0
+curr_line = 1
+
 while True:
 
   if curr_t < 1:
@@ -224,26 +244,42 @@ while True:
         curr_in += 3
       if current_tick[curr_in]:
         print(curr_in, current_tick[curr_in])
-        if current_tick[curr_in] == "speak":
-          printv = current_tick[curr_in + 1]
-          curr_hed = current_tick[curr_in + 2]
+        if current_tick[curr_in] == "speak" and line_breaks == 0:
+          if len(current_tick[curr_in+1]) > 20:
+            a = line_break(current_tick[curr_in + 1])
+            first_letter = a[0]
+            second_sentence = a[1]
+
+            printv = first_letter
+            curr_line += 1
+            line_breaks = 1
+            print(line_breaks)
+
+          else:
+            printv = current_tick[curr_in + 1]
+            line_breaks = 0
+
           curr_t += 400 + len(printv) ^ 3
-          
+          curr_hed = current_tick[curr_in + 2]
         elif current_tick[curr_in] == "clear":
           printv = ""
         elif current_tick[curr_in] != "end":
           curr_t = current_tick[curr_in + 1]
     else:
       curr_in = len(current_tick) - 1
-  p = write(printv, font)
+  if line_breaks == 1:
+    printv = second_sentence
+    line_breaks = 0
+  else:
+    p = write(printv, font)
   d = head(curr_hed)
   screen.fill((135, 206, 235))
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       pygame.quit()
 
-  b = tick(rounds_move, cool_move, cool_idle, rounds_idle, ax_move, cool_ax, 
-           god_move,cool_god)
+  b = tick(rounds_move, cool_move, cool_idle, rounds_idle, ax_move, cool_ax,
+           god_move, cool_god)
 
   rounds_move = b[0]
   rounds_idle = b[1]
@@ -269,14 +305,16 @@ while True:
     a = axe_swing(ax_move)
     screen.blit(a, (x - 40, y - 50))
   elif current_tick[curr_in] == "god enter":
-    a = god_enter(god_move,g_y)
+    a = god_enter(god_move, g_y)
     screen.blit(a, (g_x, g_y))
-    g_y +=50
-
+    g_y += 50
   elif current_tick[curr_in] == "wait" or current_tick[
       curr_in] == "end" or current_tick[curr_in] == "speak":
     a = idle(rounds_idle)
     screen.blit(a, (x, y))
+  if god_entered == 1:
+    a = images_g[0]
+    screen.blit(a, (g_x, g_y))
 
   for i in range(47):
     screen.blit(b, (posa, 568))
@@ -295,17 +333,17 @@ while True:
       screen.blit(d, (-55, -70))
   if tree_created == 1:
     screen.blit(tree, (300, 266))
-    
 
   pygame.display.flip()
-  if current_tick[curr_in] != "axe_swing" or current_tick[curr_in] != "god enter":
+  if current_tick[curr_in] != "axe_swing" or current_tick[
+      curr_in] != "god_enter" or line_breaks != 1:
     curr_t -= 1
   else:
-    if ax_move == 0:  
+    if ax_move == 0:
       print("gp")
       curr_t -= 1
       tree_created = 0
-    if curr_in == 0:
+    if god_move == 0:
       print("og")
       curr_t -= 1
-      god_entered = 0
+      god_entered = 1
